20f2f006b17ba5686ffb15c74b2ecf71cf5c7e82
Anatolij Goga <anzefal@gmail.com>
2022-12-29 21:44:03 +0200

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+import '../css/common.css';
 
+const bodyElement = document.querySelector('body');
+const randomColorStartButton = document.querySelector('button[data-start]');
+const randomColorStopButton = document.querySelector('button[data-stop]');
+const COLOR_CHANGE_DELAY = 1000;
+let timerId = null;
+randomColorStopButton.disabled = true;
+
+randomColorStartButton.addEventListener('click', runRandomColor);
+randomColorStopButton.addEventListener('click', stopRandomColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function backgroundColorChange() {
+  bodyElement.style.backgroundColor = getRandomHexColor();
+}
+
+function runRandomColor() {
+  randomColorStartButton.disabled = true;
+  randomColorStopButton.disabled = false;
+  timerId = setInterval(backgroundColorChange, COLOR_CHANGE_DELAY);
+}
+
+function stopRandomColor() {
+  randomColorStartButton.disabled = false;
+  randomColorStopButton.disabled = true;
+  clearInterval(timerId);
+}
